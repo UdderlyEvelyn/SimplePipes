2683959d427f06d23efb83977711f898ec13f748
Evelyn <udderlyevelyn@gmail.com>
2021-12-27 17:47:12 -0500

    Register/Deregister FluidUser in overrides.

--- 1.3/Source/SimplePipes/FluidUser.cs
+++ 1.3/Source/SimplePipes/FluidUser.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
 
 namespace UdderlyEvelyn.SimplePipes
 {
     public class FluidUser : Pipe
     {
         public float AmountPerTick;
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            map.GetComponent<MapComponent_SimplePipes>().RegisterUser(this);
+        }
+
+        public override void Destroy(DestroyMode mode = DestroyMode.Vanish)
+        {
+            Map.GetComponent<MapComponent_SimplePipes>().DeregisterUser(this);
+            base.Destroy(mode);
+        }
     }
 }
